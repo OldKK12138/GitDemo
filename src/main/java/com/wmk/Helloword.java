4175e733f7fe98d97208162d7e2243023b46436f
wmk <mingkangw@163.com>
2023-05-24 22:02:52 +0800

    版本3

--- src/main/java/com/wmk/Helloword.java
+++ src/main/java/com/wmk/Helloword.java
@@ -4,5 +4,6 @@ public class Helloword {
     public static void main(String[] args) {
         System.out.println("你好");
         System.out.println("hello world");
+        System.out.println("版本3");
     }
 }
